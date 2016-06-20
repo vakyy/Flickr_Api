ce615eb6ae3c6a0d17705c2599be675a641c1a80
vakyy <vasil.andreev@gmail.com>
2016-06-20 10:23:11 +0300

    Fixing a bug. The highlighting function was finding case insensitive searchings and replacing them with the searched ones. Which was confusing the search function.

--- models/htmlBuilds/highlightingSearched_FoundTexts.js
+++ models/htmlBuilds/highlightingSearched_FoundTexts.js
@@ -1,6 +1,6 @@
 function highlight(str, className, idName, element)
 {
-    var regex = new RegExp(str, "gi");
+    var regex = new RegExp(str, "g");
     return element.each(function () {
         $(this).contents().filter(function() {
             return this.nodeType == 3 && regex.test(this.nodeValue);
